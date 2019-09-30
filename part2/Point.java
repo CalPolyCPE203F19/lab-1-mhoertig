997f678779c8bde50f59a8ba6758b2ac5ac4d09a
Matt Hoertig <mhoertig@calpoly.edu>
2019-09-30 12:34:38 -0700

    Third Commit

--- part2/Point.java
+++ part2/Point.java
@@ -26,7 +26,30 @@ class Point {
 
    public double getAngle()
    {
+   if (x > 0)   
       return Math.atan(y/x);
+   if (x < 0) 
+   {
+      if (y < 0)
+         return Math.atan(y/x) - Math.PI
+      if (y > 0) 
+         return Math.atan(y/x) + Math.PI
+   }
+   if (x == 0) 
+   { 
+      if (y > 0)
+         return Math.PI / 2;
+      if (y < 0)
+         return -(Math.PI / 2);
+   }
+   if (y == 0)
+   { 
+      if (x > 0) 
+         return 0;
+      if (x < 0)
+         return Math.PI;
+   }
+   return 0
    }
 
    public Point rotate90()
